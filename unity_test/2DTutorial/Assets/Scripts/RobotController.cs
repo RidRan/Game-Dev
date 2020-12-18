e2e2600d07a44a7d33a4ae97efafe6c714927605
RidRan <riddock.moran@huskers.unl.edu>
2020-12-17 23:46:58 -0600

    Adding in progress Unity Tutorial

---
+++ unity_test/2DTutorial/Assets/Scripts/RobotController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour {
+    public float horizontalSpeed = 3f, verticalSpeed = 3f;
+    public bool horizontal, vertical;
+    public float changeTime = 3.0f;
+
+    Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start() {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        timer -= Time.deltaTime;
+
+        if (timer < 0) {
+            direction *= -1;
+            timer = changeTime;
+        }
+    }
+
+    void FixedUpdate() {
+        Vector2 position = rigidbody2D.position;
+
+        if (vertical) {
+            position.y += Time.deltaTime * verticalSpeed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+        } else {
+            position.x += Time.deltaTime * horizontalSpeed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
+        }
+        
+        rigidbody2D.MovePosition(position);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) { 
+        RubyController player = collision.gameObject.GetComponent<RubyController>();
+
+        if (player != null) {
+            player.ChangeHealth(-1);
+        }
+    }
+}
---
+++ unity_test/2DTutorial/Assets/Scripts/RubyController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour {
+    public int maxHealth = 5;
+    int currentHealth;
+    public int health {
+        get {
+            return currentHealth;
+        }
+    }
+
+    Rigidbody2D rigidbody2d;
+
+    float horizontal, vertical;
+    public float horizontalSpeed = 3f, verticalSpeed = 3f;
+
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
+    // Start is called before the first frame update
+    void Start() {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth - 2;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0) {
+                isInvincible = false;
+            }
+        }
+    }
+
+    void FixedUpdate() {
+        float frameTime = Time.deltaTime;
+
+        Vector2 position = rigidbody2d.position;
+        position.x += horizontalSpeed * horizontal * frameTime;
+        position.y += verticalSpeed * vertical * frameTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount) {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log("Health: " + currentHealth + "/" + maxHealth);
+    }
+}
